c30c7ab222851687c58efd7599953c73e0b7cc30
Shakti <127021350+ShaktiPatidar@users.noreply.github.com>
2023-09-28 11:10:50 +0530

    Add files via upload

---
+++ DataBaseConnection.java
@@ -0,0 +1,29 @@
+package software;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class DataBaseConnection {
+ 
+public static Connection dbConn() {
+
+String url = "jdbc:mysql://localhost:3306/membinfo";
+String user ="Admin";
+String pass ="Mysql@01";
+
+try {
+
+Connection conn = DriverManager.getConnection(url,user,pass);
+return conn;
+
+} catch (SQLException e) {
+
+JOptionPane.showMessageDialog(null, "Connection Failed");
+e.printStackTrace();
+return null;
+
+}
+}
+}
